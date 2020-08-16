0259482699c0830a057f7835b655caad2f53a283
guihRovetta <guilherme020213@hotmail.com>
2020-08-16 15:05:00 -0300

    :recycle: changed to styled components

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import api from '../../services/api';
+
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 
@@ -8,8 +10,13 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-import './styles.css';
-import api from '../../services/api';
+import {
+  Container,
+  Content,
+  LogoContainer,
+  ButtonsContainer,
+  TotalConnections,
+} from './styles';
 
 function Landing() {
   const [totalConnections, setTotalConnections] = useState(0);
@@ -23,12 +30,12 @@ function Landing() {
   }, []);
 
   return (
-    <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div className="logo-container">
+    <Container>
+      <Content>
+        <LogoContainer>
           <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
-        </div>
+        </LogoContainer>
 
         <img
           src={landingImg}
@@ -36,7 +43,7 @@ function Landing() {
           className="hero-image"
         />
 
-        <div className="buttons-container">
+        <ButtonsContainer>
           <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
@@ -46,14 +53,14 @@ function Landing() {
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar aulas
           </Link>
-        </div>
+        </ButtonsContainer>
 
-        <span className="total-connections">
+        <TotalConnections>
           Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo"></img>
-        </span>
-      </div>
-    </div>
+        </TotalConnections>
+      </Content>
+    </Container>
   );
 }
 
